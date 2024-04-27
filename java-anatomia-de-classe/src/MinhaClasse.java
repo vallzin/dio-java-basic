7281c26a472f52fbc6a3d16ac952ca98154f50b7
vallzin <valmilson@gmail.com>
2024-04-27 19:41:23 -0300

    declarando variáveis e método

--- java-anatomia-de-classe/src/MinhaClasse.java
+++ java-anatomia-de-classe/src/MinhaClasse.java
@@ -1,14 +1,16 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        String meuNome = "Valmilson";
+        
+        String primeiroNome = "Valmilson";
+        String ultimoNome = "lucena";
 
-        int minhaIdade = 48;
+        String nomeCompleto = nomeCompleto(primeiroNome, ultimoNome);
 
-        boolean verdadeira = true;
+        System.out.println(nomeCompleto);
+    }
 
-        verdadeira = false;
-
-        System.out.println("meu nome é " + meuNome + "e minha idade é "+ minhaIdade + "está certo ou falso: "+ verdadeira);
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "\nResultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
